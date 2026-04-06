d09d7dd6e632c98a6c5db33f2ad26d9108281e76
yuuki <yuki.tsubo@gmail.com>
2026-04-06 11:54:58 +0900

    refactor: replace hand-rolled utilities with stdlib in aggregator
    Remove dead `attrs` map field that was written but never read, eliminating
    redundant per-event cloneAttrs allocations. Replace formatUint32/formatUint64
    with strconv.FormatUint, stringsSplitNul with strings.Split, and string
    concatenation in buildMetricKey with strings.Builder. Build attribute maps
    directly in Consume to avoid intermediate map allocations via mergeAttrs.

--- internal/goexporter/aggregate.go
+++ internal/goexporter/aggregate.go
@@ -1,37 +1,39 @@
 package goexporter
 
-import "sort"
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
 
 type Aggregator struct {
 	counters   map[string]float64
 	histograms map[string][]float64
-	attrs      map[string]map[string]string
 }
 
 func NewAggregator() *Aggregator {
 	return &Aggregator{
 		counters:   map[string]float64{},
 		histograms: map[string][]float64{},
-		attrs:      map[string]map[string]string{},
 	}
 }
 
 func (a *Aggregator) Consume(event Event) {
-	baseAttrs := map[string]string{
-		"user.id":           formatUint32(event.UID),
-		"process.name":      event.Comm,
-		"lustre.actor.type": ClassifyActorType(event.Comm),
-	}
+	uid := strconv.FormatUint(uint64(event.UID), 10)
+	actorType := ClassifyActorType(event.Comm)
 
 	if event.Plane == PlaneLLite {
 		accessClass := AccessClassForOp(event.Op)
 		if accessClass == "" {
 			return
 		}
-		attrs := mergeAttrs(baseAttrs, map[string]string{
+		attrs := map[string]string{
+			"user.id":             uid,
+			"process.name":        event.Comm,
+			"lustre.actor.type":   actorType,
 			"lustre.access.class": accessClass,
 			"lustre.access.op":    event.Op,
-		})
+		}
 		a.addCounter("lustre.client.access.operations", 1, attrs)
 		return
 	}
@@ -40,16 +42,23 @@ func (a *Aggregator) Consume(event Event) {
 		return
 	}
 	if event.Op == OpQueueWait {
-		attrs := mergeAttrs(baseAttrs, map[string]string{
-			"lustre.access.op": event.Op,
-		})
+		attrs := map[string]string{
+			"user.id":           uid,
+			"process.name":      event.Comm,
+			"lustre.actor.type": actorType,
+			"lustre.access.op":  event.Op,
+		}
 		a.addCounter("lustre.client.rpc.wait.operations", 1, attrs)
 		if event.DurationUS > 0 {
 			a.addHistogram("lustre.client.rpc.wait.duration", float64(event.DurationUS), attrs)
 		}
 		return
 	}
-	attrs := baseAttrs
+	attrs := map[string]string{
+		"user.id":           uid,
+		"process.name":      event.Comm,
+		"lustre.actor.type": actorType,
+	}
 	switch event.Op {
 	case OpSendNewReq:
 		a.addCounter("lustre.client.inflight.requests", 1, attrs)
@@ -81,7 +90,7 @@ func (a *Aggregator) Collect() []AggregatedMetric {
 			Type:       metricType,
 			Unit:       unit,
 			Value:      a.counters[key],
-			Attributes: cloneAttrs(attrs),
+			Attributes: attrs,
 		})
 	}
 
@@ -98,26 +107,23 @@ func (a *Aggregator) Collect() []AggregatedMetric {
 			Type:       "histogram",
 			Unit:       "us",
 			Histogram:  values,
-			Attributes: cloneAttrs(attrs),
+			Attributes: attrs,
 		})
 	}
 
 	a.counters = map[string]float64{}
 	a.histograms = map[string][]float64{}
-	a.attrs = map[string]map[string]string{}
 	return metrics
 }
 
 func (a *Aggregator) addCounter(name string, value float64, attrs map[string]string) {
 	key := buildMetricKey(name, attrs)
 	a.counters[key] += value
-	a.attrs[key] = cloneAttrs(attrs)
 }
 
 func (a *Aggregator) addHistogram(name string, value float64, attrs map[string]string) {
 	key := buildMetricKey(name, attrs)
 	a.histograms[key] = append(a.histograms[key], value)
-	a.attrs[key] = cloneAttrs(attrs)
 }
 
 func buildMetricKey(name string, attrs map[string]string) string {
@@ -126,67 +132,23 @@ func buildMetricKey(name string, attrs map[string]string) string {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
-	out := name
+	var b strings.Builder
+	b.WriteString(name)
 	for _, key := range keys {
-		out += "\x00" + key + "\x00" + attrs[key]
+		b.WriteByte(0)
+		b.WriteString(key)
+		b.WriteByte(0)
+		b.WriteString(attrs[key])
 	}
-	return out
+	return b.String()
 }
 
 func splitMetricKey(key string) (string, map[string]string) {
-	parts := stringsSplitNul(key)
+	parts := strings.Split(key, "\x00")
 	name := parts[0]
 	attrs := map[string]string{}
 	for i := 1; i+1 < len(parts); i += 2 {
 		attrs[parts[i]] = parts[i+1]
 	}
 	return name, attrs
-}
-
-func stringsSplitNul(in string) []string {
-	var out []string
-	last := 0
-	for i := 0; i < len(in); i++ {
-		if in[i] != 0 {
-			continue
-		}
-		out = append(out, in[last:i])
-		last = i + 1
-	}
-	out = append(out, in[last:])
-	return out
-}
-
-func mergeAttrs(left map[string]string, right map[string]string) map[string]string {
-	out := cloneAttrs(left)
-	for key, value := range right {
-		out[key] = value
-	}
-	return out
-}
-
-func cloneAttrs(in map[string]string) map[string]string {
-	out := make(map[string]string, len(in))
-	for key, value := range in {
-		out[key] = value
-	}
-	return out
-}
-
-func formatUint32(v uint32) string {
-	return formatUint64(uint64(v))
-}
-
-func formatUint64(v uint64) string {
-	if v == 0 {
-		return "0"
-	}
-	var buf [20]byte
-	i := len(buf)
-	for v > 0 {
-		i--
-		buf[i] = byte('0' + (v % 10))
-		v /= 10
-	}
-	return string(buf[i:])
 }
